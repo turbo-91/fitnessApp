bd44d46a97626afe0fc52b43453de27820f36bbb
Torben Jost <torbenjost@proton.me>
2025-01-15 16:47:29 +0100

    start test server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
 
-  return (
+    const fetchData = () => {
+        axios
+            .get<[]>("http://localhost:8080/api/hello") // Ensure the response matches the type
+            .then((response) => {
+                setData(response.data); // Save the fetched data
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+
+    return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <p>{data}</p>
     </>
   )
 }
