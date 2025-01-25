a6d43371b00461a276cc30a3ea35e312ae146de6
Torben Jost <torbenjost@proton.me>
2025-01-25 09:43:11 +0100

    add optional Cardio object to Workout type in Frontend

--- frontend/src/types/Workout.ts
+++ frontend/src/types/Workout.ts
@@ -1,8 +1,10 @@
 import { Exercise} from "./Exercise.ts";
+import {Cardio} from "./Cardio.ts";
 
 export interface Workout {
     id: string;
     name: string;
     exercises: Exercise[];
+    cardio?: Cardio;
     timestamp: number;
 }
